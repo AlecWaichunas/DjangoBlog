12bbb1ea0a5569887ba42ec88dc534260e2ce9b7
Alec Waichunas <awaichunas@gmail.com>
2016-10-11 09:12:40 -0500

    HTML changes

--- myblog/App/models.py
+++ myblog/App/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class blog(models.Model):
+    blog_title = models.CharField(max_length=100)
+    blog_text = models.CharField(max_length=none)
+    blog_date = models.DateField()
+    
