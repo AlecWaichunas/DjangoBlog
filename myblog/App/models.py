bf2f1daae82726fd1480ac9d7702a8beb6f5988f
Alec Waichunas <awaichunas@gmail.com>
2016-10-12 11:47:47 -0500

    Add blog to database and index.html was updated so it shows a preview of each blog!

--- myblog/App/models.py
+++ myblog/App/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
 # Create your models here.
-class blog(models.Model):
+class Blog(models.Model):
     blog_title = models.CharField(max_length=100)
-    blog_text = models.CharField(max_length=none)
-    blog_date = models.DateField()
-    
+    blog_text = models.CharField(max_length=5000)
+    blog_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.blog_title
+
+    def get_preview(self):
+        return self.blog_text[:500]
--- myblog/App/views.py
+++ myblog/App/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from App.models import Blog
 
 # Create your views here.
 def index_view(request):
     context = {
-        'site_name': 'Alec\'s blog'
+        'site_name': 'Alec\'s blog',
+        'blogs': Blog.objects.all()
     }
     return render(request, 'index.html', context)
